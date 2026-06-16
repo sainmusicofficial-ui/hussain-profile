a0fcf3228dc91ae4adf57a91879650c69d5e15f3
sainmusicofficial-ui <sainmusic.official@gmail.com>
2026-06-16 19:07:35 +0530

    Added sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://hussainkhan.co.in",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://hussainkhan.co.in/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://hussainkhan.co.in/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://hussainkhan.co.in/lab",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://hussainkhan.co.in/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
